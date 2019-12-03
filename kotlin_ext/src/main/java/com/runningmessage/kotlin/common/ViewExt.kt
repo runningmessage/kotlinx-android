9c6a860f787c58ba0e6390510bcc6b53ad91b5a8
yaqingzhang <zhang_yaqing@sina.cn>
2019-12-03 16:48:05 +0800

    [Common] rename ViewExt.method to ViewExt.layoutMethod, add layoutWidth layoutHeight

--- kotlin_ext/src/main/java/com/runningmessage/kotlin/common/ViewExt.kt
+++ kotlin_ext/src/main/java/com/runningmessage/kotlin/common/ViewExt.kt
@@ -7,23 +7,33 @@ import android.view.ViewGroup
  * Created by Lorss on 19-3-4.
  */
 
-var <V : View> V.leftMargin: Int?
+var <V : View> V.layoutLeftMargin: Int?
     get() = (layoutParams as? ViewGroup.MarginLayoutParams)?.leftMargin
     set(value) {
         (layoutParams as? ViewGroup.MarginLayoutParams)?.leftMargin = value
     }
-var <V : View> V.rightMargin: Int?
+var <V : View> V.layoutRightMargin: Int?
     get() = (layoutParams as? ViewGroup.MarginLayoutParams)?.rightMargin
     set(value) {
         (layoutParams as? ViewGroup.MarginLayoutParams)?.rightMargin = value
     }
-var <V : View> V.topMargin: Int?
+var <V : View> V.layoutTopMargin: Int?
     get() = (layoutParams as? ViewGroup.MarginLayoutParams)?.topMargin
     set(value) {
         (layoutParams as? ViewGroup.MarginLayoutParams)?.topMargin = value
     }
-var <V : View> V.bottomMargin: Int?
+var <V : View> V.layoutBottomMargin: Int?
     get() = (layoutParams as? ViewGroup.MarginLayoutParams)?.bottomMargin
     set(value) {
         (layoutParams as? ViewGroup.MarginLayoutParams)?.bottomMargin = value
+    }
+var <V : View> V.layoutWidth: Int?
+    get() = layoutParams?.width
+    set(value) {
+        layoutParams?.width = value
+    }
+var <V : View> V.layoutHeight: Int?
+    get() = layoutParams?.height
+    set(value) {
+        layoutParams?.height = value
     }
