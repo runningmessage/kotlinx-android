ec3d5a9aa33df89dc9e1587f421667cdf71271ae
yaqingzhang <zhang_yaqing@sina.cn>
2020-01-13 10:19:17 +0800

    [kotlinx-reflect] remove unused code

--- kotlinx-reflect/src/main/java/com/runningmessage/kotlinx/reflect/ReflectException.java
+++ kotlinx-reflect/src/main/java/com/runningmessage/kotlinx/reflect/ReflectException.java
@@ -13,25 +13,6 @@
  */
 package com.runningmessage.kotlinx.reflect;
 
-import java.lang.reflect.InvocationTargetException;
-
-/**
- * A unchecked wrapper for any of Java's checked reflection exceptions:
- * <p>
- * These exceptions are
- * <ul>
- * <li> {@link ClassNotFoundException}</li>
- * <li> {@link IllegalAccessException}</li>
- * <li> {@link IllegalArgumentException}</li>
- * <li> {@link InstantiationException}</li>
- * <li> {@link InvocationTargetException}</li>
- * <li> {@link NoSuchMethodException}</li>
- * <li> {@link NoSuchFieldException}</li>
- * <li> {@link SecurityException}</li>
- * </ul>
- *
- * @author Lukas Eder
- */
 public class ReflectException extends RuntimeException {
 
     /**
